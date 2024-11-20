3a9ce3d7f2505c84a4815146b51407806157786f
Alejandra Palza Morales <alejandra.palza.morales@gmail.com>
2024-11-20 16:16:35 -0400

    Refactorizando las funciones

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,25 +1,47 @@
 function calcularSumaCadena(cadena) {
-    if(cadena.length === 0){
+    if (esCadenaVacia(cadena)) {
       return 0;
     }
   
-    let delimitadores = /[,-]/;
-    let numerosCadena = cadena;
+    let delimitadores = obtenerDelimitadores(cadena);
+    let cadenaNumeros = extraerCadenaNumeros(cadena, delimitadores);
   
-    if(cadena.startsWith("//")){
-      const delimitadorEspesificado = cadena[3];
-      delimitadores = new RegExp(`[${delimitadorEspesificado},-]`);
-      numerosCadena = cadena.substring(6);
+    const numeros = dividirCadenaEnNumeros(cadenaNumeros, delimitadores);
+  
+    return sumarNumeros(numeros);
+  }
+  
+  function esCadenaVacia(cadena) {
+    return cadena.length === 0;
+  }
+  
+  function obtenerDelimitadores(cadena) {
+    if (cadena.startsWith("//")) {
+      const delimitadorEspecificado = cadena[3];
+      return new RegExp(`[${delimitadorEspecificado},-]`);
     }
+    return /[,-]/;
+  }
   
-    const numeros = numerosCadena.split(delimitadores);
+  function extraerCadenaNumeros(cadena, delimitadores) {
+    if (cadena.startsWith("//")) {
+      return cadena.substring(6);
+    }
+    return cadena;
+  }
   
+  function dividirCadenaEnNumeros(cadenaNumeros, delimitadores) {
+    return cadenaNumeros.split(delimitadores);
+  }
+  
+  function sumarNumeros(numeros) {
     let suma = 0;
-    for (let indice = 0; indice < numeros.length; indice++) {
-      if(numeros[indice] <= 1000)
-        suma += parseInt(numeros[indice]);
+    for (let i = 0; i < numeros.length; i++) {
+      const numero = parseInt(numeros[i]);
+      if (numero <= 1000) {
+        suma += numero;
+      }
     }
-  
     return suma;
   }
   

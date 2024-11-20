7b18a30d9f44d3a1a04bb2a0ace2b14e85a2efd4
Alejandra Palza Morales <alejandra.palza.morales@gmail.com>
2024-11-20 15:27:23 -0400

    Prueba de test.js Si la cadena es vacia debe retornar 0

---
+++ src/calculadora.test.js
@@ -0,0 +1,8 @@
+import calcularSumaCadena from "./calculadora.js";
+
+describe("Sumar digitos", () => {
+  it("Si es vacia la cadena debe retornar 0", () => {
+    expect(calcularSumaCadena("")).toEqual(0);
+  });
+});
+
